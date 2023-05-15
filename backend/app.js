9323a30f2dc19c2e8d191b33b84f8a9a7147b1e5
glopez <glopez-dev@proton.me>
2023-05-15 18:45:23 +0200

    Refactor : extraction de la logique des controllers hors des routes

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const Thing = require('./models/Thing');
-const app = express();
+const stuffRoutes = require('./routes/stuff');
 
 // Connexion au serveur MongoDB
 const password = encodeURIComponent("5HP1sdVI6tY9PmC0");
@@ -12,48 +11,7 @@ mongoose.connect(`mongodb+srv://oc_fullstack:${password}@cluster0.nhdmyss.mongod
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
 
-// Middlewares
-app.use(express.json());
-
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-    next();
-});
-
-app.post('/api/stuff', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then( () => res.status(201).json({message: 'Objet enregistré'}) )
-        .catch( (error) => req.status(400).json({error}) );
-});
-
-app.get('/api/stuff', (req, res, next) => {
-    Thing.find()
-        .then( (things) => res.status(200).json(things) )
-        .catch( (error) => res.status(400).json({error}) );
-});
-
-app.get('/api/stuff/:id', (req, res, next) => {
-    Thing.findOne( {_id: req.params.id} )
-        .then( (thing) => res.status(200).json(thing) )
-        .catch ( (error) => res.status(400).json({error}) );
-});
-
-app.put('/api/stuff/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-    .then( () => res.status(200).json({ message: 'Objet modifié'}) )
-    .catch( (error) => res.status(400).json({error}) );
-});
-
-app.delete('/api/stuff/:id', (req, res, next) => {
-  Thing.deleteOne( {_id: req.params.id} )
-    .then( () => res.status(200).json({message: 'Objet supprimé'}) )
-    .catch( (error) => res.status(400).json({error}) );
-});
+const app = express();
+app.use('/api/stuff', stuffRoutes);
 
 module.exports = app;
--- backend/routes/stuff.js
+++ backend/routes/stuff.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Import du modèle "Thing"
-const Thing = require('./models/Thing');
+const stuffController = require('../controllers/stuff.js');
 
 // Middlewares
 router.use(express.json());
@@ -14,39 +14,15 @@ router.use((req, res, next) => {
     next();
 });
 
-router.post('/', (req, res, next) => {
-    delete req.body._id;
-    const thing = new Thing({
-        ...req.body
-    });
-    thing.save()
-        .then( () => res.status(201).json({message: 'Objet enregistré'}) )
-        .catch( (error) => req.status(400).json({error}) );
-});
+router.post('/', stuffController.createThing);
 
-router.get('/' + '', (req, res, next) => {
-    Thing.find()
-        .then( (things) => res.status(200).json(things) )
-        .catch( (error) => res.status(400).json({error}) );
-});
+router.get('/' + '', stuffController.getAllThings);
 
-router.get('/:id', (req, res, next) => {
-    Thing.findOne( {_id: req.params.id} )
-        .then( (thing) => res.status(200).json(thing) )
-        .catch ( (error) => res.status(400).json({error}) );
-});
+router.get('/:id', stuffController.getOneThing);
 
-router.put('/:id', (req, res, next) => {
-  Thing.updateOne({ _id: req.params.id }, {...req.body, _id: req.params.id })
-    .then( () => res.status(200).json({ message: 'Objet modifié'}) )
-    .catch( (error) => res.status(400).json({error}) );
-});
+router.put('/:id', stuffController.modifyThing);
 
-router.delete('/:id', (req, res, next) => {
-  Thing.deleteOne( {_id: req.params.id} )
-    .then( () => res.status(200).json({message: 'Objet supprimé'}) )
-    .catch( (error) => res.status(400).json({error}) );
-});
+router.delete('/:id', stuffController.deleteThing);
 
 
 module.exports = router;
